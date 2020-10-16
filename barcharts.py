873f1ea6263662aee1a5ed467c1b96bacf4f06f6
giuliavilone <giulia.vilone@gmail.com>
2020-10-16 12:03:13 +0100

    improved version of friedman_test.py and barcharts.py

--- barcharts.py
+++ barcharts.py
@@ -3,9 +3,37 @@ import matplotlib
 import matplotlib.pyplot as plt
 matplotlib.use('TkAgg')
 
-data = pd.read_csv('dataplot.csv')
-groups = data.groupby(['Metrics'], as_index=False)
-for index, item in groups:
-    ax = item.plot.bar(x='Method', rot=0, title=index)
+by_method = False
+if by_method:
+    data = pd.read_csv('dataplot.csv')
+    groups = data.groupby(['Metrics'], as_index=False)
+    for index, item in groups:
+        ax = item.plot.bar(x='Method', rot=0)
+        ax.set_title(index, fontsize=18)
+        ax.legend(loc="best", fontsize=12)
+        ax.get_legend().remove()
+        ax.plot()
+        plt.show()
+    ranks = pd.read_csv('all_ranks.csv')
+    ax = ranks.plot.bar(x='Method', rot=0)
+    ax.set_title('Method ranks', fontsize=18)
+    ax.legend(loc="best", fontsize=12, bbox_to_anchor=(1, 1))
+    ax.plot()
+    plt.show()
+else:
+    data = pd.read_csv('dataplot2.csv')
+    groups = data.groupby(['Metrics'], as_index=False)
+    for index, item in groups:
+        ax = item.plot.bar(x='Database', rot=18)
+        ax.set_title(index, fontsize=18)
+        ax.legend(loc="best", fontsize=12)
+        #ax.get_legend().remove()
+        ax.plot()
+        plt.show()
+    ranks = pd.read_csv('all_ranks2.csv')
+    ax = ranks.plot.bar(x='Dataset', rot=18, fontsize=14)
+    ax.set_title('Total ranks', fontsize=18)
+    ax.set_ylabel('Sum of ranks', fontsize=14)
+    ax.legend(loc="best", fontsize=14, bbox_to_anchor=(1, 1))
     ax.plot()
     plt.show()
--- friedman_test.py
+++ friedman_test.py
@@ -1,18 +1,37 @@
 from scipy.stats import friedmanchisquare
 import pandas as pd
-import numpy as np
+from scipy.stats import rankdata
+
+
+def method_ranking(df, dataset):
+    outlist = []
+    for label, col in df.iteritems():
+        if (label == 'Method') or (label == 'Dataset'):
+            col = col.tolist()
+            col.insert(0, label)
+            outlist.append(col)
+        else:
+            col = col.to_numpy()
+            ranks = rankdata(col).tolist()
+            #if (label == 'n_rules') or (label == 'avg_length'):
+            #    ranks = list(map(lambda x: 5-x, ranks))
+            ranks.insert(0, label)
+            outlist.append(ranks)
+        outdf = pd.DataFrame(outlist, columns=['item', 'rank1', 'rank2', 'rank3', 'rank4'])
+        outdf.to_csv('ranks_'+dataset+'.csv')
+
 
 data = pd.read_csv('metrics.csv')
+method_groups = data.groupby(['Dataset'], as_index=False)
 
-method_groups = data.groupby(['Method'], as_index=False)
 
 method_list = []
 for index, item in method_groups:
+    print(index)
+    method_ranking(item, index)
     item = item.drop(['Method', 'Dataset'], axis=1)
     item = item.to_numpy()
-    method_list.append(item)
+    data1, data2, data3, data4 = item
+    stat, p = friedmanchisquare(data1, data2, data3, data4)
+    print('Friedman statistics=%.3f, p=%.3f' % (stat, p))
 
-data1, data2, data3, data4 = method_list
-
-stat, p = friedmanchisquare(data1, data2, data3, data4)
-print('Statistics=%.3f, p=%.3f' % (stat, p))
