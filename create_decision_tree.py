4f0011dd93abd8293de0af07e178355cf880e647
giuliavilone <giulia.vilone@gmail.com>
2021-03-30 19:10:59 +0100

    first production version of create_decision_tree.py

--- create_decision_tree.py
+++ create_decision_tree.py
@@ -3,7 +3,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 import dash_table
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import pandas as pd
 import numpy as np
 from common_functions import dataset_uploader
@@ -12,13 +12,18 @@ from keras.models import load_model
 import dash_cytoscape as cyto
 
 
-def create_edges(decision_tree):
-    edges_left = [{'data': {'source': str(i), 'target': decision_tree.tree_.children_left[i]}, 'classes': 'green'}
-                  for i in range(len(decision_tree.tree_.children_left)) if decision_tree.tree_.children_left[i] != -1]
-    edges_right = [{'data': {'source':  str(i), 'target': decision_tree.tree_.children_right[i]}, 'classes': 'red'}
-                   for i in range(len(decision_tree.tree_.children_right))
-                   if decision_tree.tree_.children_right[i] != -1]
-    return edges_left, edges_right
+def create_edges(tree_node_list, touched_nodes, color_name):
+    ret = []
+    for i in range(len(tree_node_list)):
+        if tree_node_list[i] != -1:
+            if i in touched_nodes and tree_node_list[i] in touched_nodes:
+                ret += [{'data': {'source': str(i), 'target': tree_node_list[i]}, 'classes': 'orange'}]
+            else:
+                if len(touched_nodes) > 0:
+                    ret += [{'data': {'source': str(i), 'target': tree_node_list[i]}, 'classes': 'greyed_out'}]
+                else:
+                    ret += [{'data': {'source': str(i), 'target': tree_node_list[i]}, 'classes': color_name}]
+    return ret
 
 
 def retrieve_fired_nodes(decision_tree, sample):
@@ -26,38 +31,43 @@ def retrieve_fired_nodes(decision_tree, sample):
     return list(node_indicator.indices[node_indicator.indptr[0]:node_indicator.indptr[1]])
 
 
-def create_node_labels(in_df, col_names, class_labels, decision_tree, separator=' <= '):
+def create_node_labels(col_names, class_labels, decision_tree, separator=' <= '):
     values = [class_labels[np.where(arr[0] == np.amax(arr[0]))[0][0]] for arr in decision_tree.tree_.value]
     names = [str(col_names[decision_tree.tree_.feature[i]]) + separator + str(round(decision_tree.tree_.threshold[i], 2))
              if decision_tree.tree_.feature[i] != -2 else values[i] for i in range(len(values))]
     return names
 
 
-def create_network(in_df, class_labels, col_names, decision_tree, sample=[], **kwargs):
+def create_network(class_labels, col_names, decision_tree, sample=[], **kwargs):
     if len(sample) > 0:
         touched_nodes = retrieve_fired_nodes(decision_tree, sample)
     else:
         touched_nodes = []
     color_list = ['#fcc603', '#03c2fc', '#fc03db']
     ret = []
-    names = create_node_labels(in_df, col_names, class_labels, decision_tree)
+    names = create_node_labels(col_names, class_labels, decision_tree)
     for i in range(len(names)):
         if i in touched_nodes:
             ret += [{'data': {'id': str(i), 'label': names[i]}, 'classes': 'orange'}]
         else:
-            if names[i] in class_labels:
-                color_number = class_labels.index(names[i])
-                ret += [{'data': {'id': str(i), 'label': names[i]}, 'classes': 'color' + str(color_number)}]
+            if len(touched_nodes) > 0:
+                ret += [{'data': {'id': str(i), 'label': names[i]}, 'classes': 'greyed_out'}]
             else:
-                ret += [{'data': {'id': str(i), 'label': names[i]}}]
-    edges_left, edges_right = create_edges(decision_tree)
+                if names[i] in class_labels:
+                    color_number = class_labels.index(names[i])
+                    ret += [{'data': {'id': str(i), 'label': names[i]}, 'classes': 'color' + str(color_number)}]
+                else:
+                    ret += [{'data': {'id': str(i), 'label': names[i]}, 'classes': 'color_base'}]
+    edges_left = create_edges(decision_tree.tree_.children_left, touched_nodes, 'green')
+    edges_right = create_edges(decision_tree.tree_.children_right, touched_nodes, 'red')
     return ret + edges_left + edges_right
 
 
 def node_style():
-    color_list = ['#fcc603', '#03c2fc', '#fc03db']
+    color_list = ['#F923C3', '#FD611D', '#841DFD']
     node_styles = [{'selector': '.color' + str(i), 'style': {'background-color': color_list[i]}}
                    for i in range(len(color_list))]
+    node_styles +=[{'selector': '.color_base', 'style': {'background-color': '#76C5C8'}}]
     return node_styles
 
 
@@ -75,6 +85,7 @@ model = load_model('trained_models/trained_model_' + dataset_par['dataset'] + '_
                    + str(dataset_par['best_model']) + '.h5')
 x_tot, y_tot, clf = create_tree(X_train, model)
 
+graph_elements = create_network(labels, columns, clf)
 
 # app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -85,19 +96,44 @@ app.layout = html.Div(children=[
                 id='cytoscape-graph',
                 layout={'name': 'cose'},
                 style={'height': column_height},
-                elements=create_network(x_tot, labels, columns, clf),
+                elements=graph_elements,
                 stylesheet=[
                     {
                         'selector': 'node',
                         'style': {'content': 'data(label)'}
                     },
                     {
+                        'selector': 'edge',
+                        'style': {'curve-style': 'bezier'}
+                    },
+                    {
                         'selector': '.red',
-                        'style': {'line-color': 'red'}
+                        'style': {'line-color': 'red',
+                                  'target-arrow-color': 'red',
+                                  'target-arrow-shape': 'triangle'}
                     },
                     {
                         'selector': '.green',
-                        'style': {'line-color': 'green'}
+                        'style': {'line-color': 'green',
+                                  'target-arrow-color': 'green',
+                                  'target-arrow-shape': 'triangle'}
+                    },
+                    {
+                        'selector': '.orange',
+                        'style': {'background-color': 'orange',
+                                  'shape': 'rectangle',
+                                  'line-color': 'orange',
+                                  'target-arrow-color': 'orange',
+                                  'target-arrow-shape': 'orange'
+                                  }
+                    },
+                    {
+                        'selector': '.greyed_out',
+                        'style': {'background-color': '#C6C6C6',
+                                  'line-color': '#C6C6C6',
+                                  'target-arrow-color': '#C6C6C6',
+                                  'target-arrow-shape': '#C6C6C6'
+                                  }
                     }] + node_style()
             )
             ]), width=7
@@ -106,7 +142,7 @@ app.layout = html.Div(children=[
             html.H4("Dataset: " + dataset_par['dataset'], style={'color': '#21618C'}),
             dcc.Tab(label='Fixed costs', children=[
                 dash_table.DataTable(
-                    id='dataset_' + dataset_par['dataset'],
+                    id='datatable-interactivity',
                     columns=[{"name": i, "id": i} for i in X_test.columns],
                     data=X_test.to_dict('records'),
                     filter_action='native',
@@ -121,6 +157,36 @@ app.layout = html.Div(children=[
         )
     ], align="centre", justify="centre", no_gutters=False)
 ])
+
+
+@app.callback(
+    Output('datatable-interactivity', 'style_data_conditional'),
+    Input('datatable-interactivity', 'selected_rows')
+)
+def update_styles(selected_rows=[]):
+    if selected_rows != None:
+        return [{
+            'if': {'row_index': i},
+            'background_color': '#D2F3FF'
+        } for i in selected_rows]
+    else:
+        return None
+
+
+@app.callback(
+    Output('cytoscape-graph', "elements"),
+    Input('datatable-interactivity', "derived_virtual_data"),
+    Input('datatable-interactivity', "derived_virtual_selected_rows"),
+    State('cytoscape-graph', "elements")
+)
+def update_graph(rows, derived_virtual_selected_rows, elements):
+    if derived_virtual_selected_rows is None:
+        derived_virtual_selected_rows = []
+    dff = None if rows is None else pd.DataFrame(rows)
+    if len(derived_virtual_selected_rows) > 0:
+        select_data = dff.iloc[derived_virtual_selected_rows].to_numpy()
+        elements = create_network(labels, columns, clf, sample=select_data)
+    return elements
 
 
 if __name__ == '__main__':
